31d6b126c9218358f19c3371de9175b632c3fdc1
Panganiban <AlvinPanganiban@artesyn.com>
2019-05-27 14:39:06 +0800

    Added a form template

--- logistics_scorecard_A/survey_app/views.py
+++ logistics_scorecard_A/survey_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("OK")
+    questions1 = Category.objects.filter(category_name="Cleanliness")
+
+    context = {
+        "questions1": questions1,
+    }
+
+    return render(request, "form.html", context)
