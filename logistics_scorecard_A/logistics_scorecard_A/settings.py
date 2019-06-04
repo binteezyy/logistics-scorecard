f382a6fdbe9e132cf49c51b677d053b8fedd4243
Panganiban <AlvinPanganiban@artesyn.com>
2019-06-04 18:00:22 +0800

    ads

--- logistics_scorecard_A/logistics_scorecard_A/settings.py
+++ logistics_scorecard_A/logistics_scorecard_A/settings.py
@@ -68,7 +68,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'users',
     'survey_app',
     # 'django_python3_ldap',
 ]
