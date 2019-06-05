0b372c9fac2add2ce6b0e1a0baa857c8bbf4b23f
unknown <alvinpanganiban22@gmail.com>
2019-06-06 04:23:05 +0800

    Modifications for NA

--- logistics_scorecard_A/survey_app/views.py
+++ logistics_scorecard_A/survey_app/views.py
@@ -6,7 +6,7 @@ from .models import *
 from django.core.mail import EmailMessage
 from django.core.mail import send_mail
 import smtplib
-
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
@@ -21,18 +21,20 @@ def index(request, cid):
         "categories": categories,
         "ratings": ratings,
     }
-    
+
     if request.method == 'POST':
         for category in categories:
             for question in category.questions.all():
-        #         # return HttpResponse(request.POST.get('cat-%s-row-%s' % (category.category_number, question.question_number)))                
-        #         # for rating in scorecard.rating.all():
-        #         #     if rating.question.question_string == question.question_string:
-        #         # Rating.objects.filter(question__question_string=question.question_string).update(rate=request.POST.get('cat-%s-row-%s' % (category.category_number, question.question_number)))
-                old_rate = scorecard.rating.get(question__question_string=question.question_string)
-                new_rate = Rating.objects.get(question__question_string=question.question_string, rate=request.POST.get('cat-%s-row-%s' % (category.category_number, question.question_number)))
+                #         # return HttpResponse(request.POST.get('cat-%s-row-%s' % (category.category_number, question.question_number)))
+                #         # for rating in scorecard.rating.all():
+                #         #     if rating.question.question_string == question.question_string:
+                #         # Rating.objects.filter(question__question_string=question.question_string).update(rate=request.POST.get('cat-%s-row-%s' % (category.category_number, question.question_number)))
+                old_rate = scorecard.rating.get(
+                    question__question_string=question.question_string)
+                new_rate = Rating.objects.get(question__question_string=question.question_string, rate=request.POST.get(
+                    'cat-%s-row-%s' % (category.category_number, question.question_number)))
                 # new_rate = Scorecard.objects.get(cid=cid, rating__question__question_string=question.question_string, rating__rate=request.POST.get('cat-%s-row-%s' % (category.category_number, question.question_number)))[0]
-                
+
                 # new_rate = request.POST.get('cat-%s-row-%s' % (category.category_number, question.question_number))
                 if old_rate != new_rate:
                     scorecard.rating.remove(old_rate)
@@ -41,11 +43,11 @@ def index(request, cid):
                 # return HttpResponse('old-%s new-%s' % (old_rate, new_rate))
         return HttpResponse("OK")
     else:
-        return render(request, "form.html", context)
-    
+        return render(request, "try.html", context)
+
 
 def email_view(request):
 
-    send_mail('test','test','realtantan7@gmail.com', ['Alvin.Panganiban@artesyn.com'])
+    send_mail('test', 'test', 'realtantan7@gmail.com',
+              ['Alvin.Panganiban@artesyn.com'])
     return HttpResponse("OK")
-
