6da79211f085420d76b148ee03b855a0103c97ec
Panganiban <AlvinPanganiban@artesyn.com>
2019-06-18 14:48:31 +0800

    "Updated, linked changes with temp-dev and ui"

--- logistics_scorecard_A/survey_app/views.py
+++ logistics_scorecard_A/survey_app/views.py
@@ -53,13 +53,12 @@ def index(request, cid):
         "scorecard": scorecard,
         "categories": categories,
         "ratings": ratings,
+        'date_now': datetime.datetime.now(),
     }
 
     if request.method == 'POST':
         for category in categories:
             for question in category.questions.all():
-                old_rate = scorecard.rating.get(
-                    question__question_string=question.question_string)
                 try:
                     new_rate = Rating.objects.get(question__question_string=question.question_string, rate=request.POST.get(
                     'cat-%s-row-%s' % (category.category_number, question.question_number)))
@@ -69,30 +68,34 @@ def index(request, cid):
                     add_rate.save()
                     new_rate = Rating.objects.get(question__question_string=question.question_string, rate=request.POST.get(
                     'cat-%s-row-%s' % (category.category_number, question.question_number)))
-                                
-                if old_rate != new_rate:
-                    scorecard.rating.remove(old_rate)
+                try:
+                    old_rate = scorecard.rating.get(
+                        question__question_string=question.question_string)
+                    if old_rate != new_rate:
+                        scorecard.rating.remove(old_rate)
+                        scorecard.rating.add(new_rate)
+                except Rating.DoesNotExist:
                     scorecard.rating.add(new_rate)
 
-                # return HtpResponse('old-%s new-%s' % (old_rate, new_rate))
-        # msg = MIMEMultipart()
-        # msg['From'] = "#"
-        # msg['To'] = scorecard.account_manager.email
-        # msg['Subject'] = "LOGISTICS MONTHLY SCORECARD"
+                # return HttpResponse('old-%s new-%s' % (old_rate, new_rate))
+        msg = MIMEMultipart()
+        msg['From'] = "#"
+        msg['To'] = scorecard.account_manager.email
+        msg['Subject'] = "LOGISTICS MONTHLY SCORECARD"
 
-        # message = "MenRTrashMenRTrashMenRTrashMenRTrashMenRTrash"
+        message = "10.162.197.88/login"
 
-        # # add in the message body
-        # msg.attach(MIMEText(message, 'plain'))
+        # add in the message body
+        msg.attach(MIMEText(message, 'plain'))
 
-        # mailserver = smtplib.SMTP('smtp.office365.com',587)
-        # mailserver.ehlo()
-        # mailserver.starttls()
-        # mailserver.login(msg['From'], 'password')
-        # mailserver.sendmail(msg['From'], msg['To'], msg.as_string())
+        mailserver = smtplib.SMTP('smtp.office365.com',587)
+        mailserver.ehlo()
+        mailserver.starttls()
+        mailserver.login(msg['From'], '#')
+        mailserver.sendmail(msg['From'], msg['To'], msg.as_string())
         scorecard.is_applicable = True
         scorecard.save()
-        return redirect('landing')
+        return HttpResponse("OK")
     else:
         return render(request, "form.html", context)
 
